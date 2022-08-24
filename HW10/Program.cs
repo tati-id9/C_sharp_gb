e9b627e1294fd6ee82cb5637f4465cda43b18fdb
Demakova Tatiana <demakova_ts@mail.ru>
2022-08-24 16:17:57 +0700

    добавлено решение 13-й задачи

--- HW10/Program.cs
+++ HW10/Program.cs
@@ -4,7 +4,7 @@
 782 -> 8
 918 -> 1 */
 
-int Func(int namber)
+/* int Func(int namber)
 {
     return Math.Abs((namber/10)%10);
 }
@@ -20,7 +20,7 @@ if ((a>=100 && a<1000) || (a<=-100 && a>-1000))
 else
 {
     Console.WriteLine ("Вы ввели не верное число, число должно быть трехзначным");
-}
+} */
 
 /* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 что третьей цифры нет.
@@ -28,7 +28,20 @@ else
 78 -> третьей цифры нет
 32679 -> 6 */
 
+Console.WriteLine ("Ввведите число:");
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+string numberS = Convert.ToString(number);
 
+if (number>99)
+{
+    string result = Convert.ToString(numberS[2]);
+    Console.WriteLine (result);
+
+}
+else
+{
+    Console.WriteLine ("Tретьей цифры нет");
+}
 
 /* Задача 15: Напишите программу, которая принимает на вход цифру, 
 обозначающую день недели, и проверяет, является ли этот день выходным.
