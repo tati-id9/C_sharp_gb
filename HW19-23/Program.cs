9afef214ed30b7d5a728263041aff42a57ffe210
Demakova Tatiana <demakova_ts@mail.ru>
2022-08-29 20:37:09 +0700

    внесены изменения в структуру проекта

--- HW19-23/Program.cs
+++ HW19-23/Program.cs
@@ -13,7 +13,7 @@ else if (task == 21)
 }
 else if (task == 23)
 {
-
+    RunGetCube();
 }
 else
 {
@@ -91,22 +91,26 @@ double Distance(double[,] array)
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 */
-Console.WriteLine ("Ввведите число:");
-int N = int.Parse(Console.ReadLine());
 
-double[] GetCube (int n)
+void RunGetCube()
+{
+    Console.WriteLine("Ввведите число:");
+    int N = int.Parse(Console.ReadLine());
+
+    Console.Write("Таблица кубов -> ");
+    foreach (int num in GetCube(N))
+    {
+        Console.Write($"{num} ");
+    }
+}
+
+double[] GetCube(int n)
 {
     double[] arr = new double[n];
 
-    for (int i=1; i<=n;i++)
+    for (int i = 0; i < n; i++)
     {
-        arr [i-1]= Math.Pow(i,3);
+        arr[i] = Math.Pow(i+1, 3);
     }
     return arr;
-}
-
-Console.Write("Таблица кубов -> ");
-foreach (int num in GetCube (N))
-{
-    Console.Write($"{num} ");
 }
