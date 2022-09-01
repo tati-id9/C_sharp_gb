bbd72751a0a8d46ba13b6f255999c4b38fd9526c
Demakova Tatiana <demakova_ts@mail.ru>
2022-09-01 16:29:53 +0700

    добавлены решения для задач 27 и 29

--- HW25-29/Program.cs
+++ HW25-29/Program.cs
@@ -9,7 +9,43 @@
 82 -> 10
 9012 -> 12 */
 
+Console.WriteLine("Ввведите число:");
+int number = int.Parse(Console.ReadLine());
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр = " + sumNumber);
+
+int SumNumber(int numberN)
+{
+
+    int count = Convert.ToString(numberN).Length;
+    int advance = 0, result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        advance = numberN - numberN % 10;
+        result = result + (numberN - advance);
+        numberN = numberN / 10;
+    }
+    return result;
+}
+
+
 /* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
+
+int[] GetArray(int N)
+{
+    int[] arr = new int[N];
+
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+
+    return arr;
+}
+System.Console.WriteLine("Ввведите число:");
+int N = int.Parse(Console.ReadLine());
+System.Console.WriteLine($"[{string.Join(", ", GetArray(N))}]");
