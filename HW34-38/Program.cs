d7f19f09e6c56d257932f6fcd2b7fce10bd3ba36
Demakova Tatiana <demakova_ts@mail.ru>
2022-09-02 02:21:27 +0700

    добавлено решение 36-й задачи

--- HW34-38/Program.cs
+++ HW34-38/Program.cs
@@ -4,23 +4,23 @@
 
     [345, 897, 568, 234] -> 2 */
 
-int [] RandomArr(int size)
+int[] RandomArr(int size)
 {
-     int[] arr = new int[size];
-     for (int i=0; i< arr.Length; i++)
-     {
-        arr[i] = new Random().Next(100,1000);
-     }
-     return arr;
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
 }
 
 int HonestNumber(int[] arr)
 {
     int count = 0;
 
-    for (int i=0; i<arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i]%2 == 0)
+        if (arr[i] % 2 == 0)
         {
             count++;
         }
@@ -28,7 +28,34 @@ int HonestNumber(int[] arr)
     return count;
 }
 
+void Task34()
+{
+    int[] array = RandomArr(5);
+    int hNumber = HonestNumber(array);
+    System.Console.WriteLine($"[{string.Join(", ", array)}]");
+    System.Console.WriteLine(hNumber);
+}
+/* Задача 36: Задайте одномерный массив, 
+заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+    [3, 7, 23, 12] -> 19
+    [-4, -6, 89, 6] -> 0 */
+int SumElements (int[] arr) //сумма элементов массива, стоящих на нечётных позициях
+{
+    int sum=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+
 int[] array = RandomArr(5);
-int hNumber= HonestNumber(array);
+int sum =SumElements(array);
 System.Console.WriteLine($"[{string.Join(", ", array)}]");
-System.Console.WriteLine(hNumber);
+System.Console.WriteLine(sum);
