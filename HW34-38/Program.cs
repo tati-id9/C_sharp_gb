f30967ca28221d698c3bb6b6fe52814e0ead514c
Demakova Tatiana <demakova_ts@mail.ru>
2022-09-02 02:06:15 +0700

    добавлено решение 34-й задачи

---
+++ HW34-38/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу,
+которая покажет количество чётных чисел в массиве.
+
+    [345, 897, 568, 234] -> 2 */
+
+int [] RandomArr(int size)
+{
+     int[] arr = new int[size];
+     for (int i=0; i< arr.Length; i++)
+     {
+        arr[i] = new Random().Next(100,1000);
+     }
+     return arr;
+}
+
+int HonestNumber(int[] arr)
+{
+    int count = 0;
+
+    for (int i=0; i<arr.Length; i++)
+    {
+        if (arr[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = RandomArr(5);
+int hNumber= HonestNumber(array);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+System.Console.WriteLine(hNumber);
