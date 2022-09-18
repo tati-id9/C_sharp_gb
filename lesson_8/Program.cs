a3f724c8b478d869fe9b7e72d88bc130fbb6a976
Demakova Tatiana <demakova_ts@mail.ru>
2022-09-18 21:03:51 +0700

    update lesson 8

--- lesson_8/Program.cs
+++ lesson_8/Program.cs
@@ -1,67 +1,79 @@
-﻿
-Console.WriteLine($"Задача по лекции 8");
-
-int n = InputNumbers("Введите количество строк: ");
-
-double[,] pascalTriangle = new double[n + 1, 2 * n + 1];
-
-FillPascalTriangle(pascalTriangle);
-WriteArray(pascalTriangle);
-TransformationPascalTriangle(pascalTriangle);
-WriteArray(pascalTriangle);
-
-int InputNumbers(string input)
+﻿using System;
+namespace Program
 {
-  System.Console.WriteLine(input);
-  int output = int.Parse(Console.ReadLine());
-  return output;
-}
-
-void TransformationPascalTriangle(double[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    int count = 0;
-    for (int j = array.GetLength(1) - 1; j >= 0; j--)
+    class MainClass
     {
-      if (array[i, j] != 0)
-      {
-        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
-        array[i, j] = 0;
-        count++;
-      }
-    }
-  }
-  array[array.GetLength(0) - 1, 0] = 1;
-}
+        public static void Main()
+        {
+            Console.WriteLine($"Задача по лекции 8");
+            
+            int n = InputNumbers("Введите количество строк: ");
 
-void FillPascalTriangle(double[,] pascalTriangle)
-{
-  for (int k = 0; k < pascalTriangle.GetLength(0); k++)
-  {
-    pascalTriangle[k, 0] = 1;
-  }
-  for (int i = 1; i < pascalTriangle.GetLength(0); i++)
-  {
-    for (int j = 1; j < i + 1; j++)
-    {
-      pascalTriangle[i, j] = pascalTriangle[i - 1, j] + pascalTriangle[i - 1, j - 1];
-    }
-  }
-}
+            double[,] pascalTriangle = new double[n + 1, 2 * n + 1];
 
-void WriteArray(double[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i, j] != 0)
-      {
-          Console.Write($"{array[i, j]} ");
-      }
-      else Console.Write("  ");
+            PascalTriangle.FillPascalTriangle(pascalTriangle);
+            WriteArray(pascalTriangle);
+            PascalTriangle.TransformationPascalTriangle(pascalTriangle);
+            WriteArray(pascalTriangle);
+            Console.ReadKey();
+        }
+        public static int InputNumbers(string input)
+        {
+            System.Console.WriteLine(input);
+            int output = int.Parse(Console.ReadLine());
+            return output;
+        }
+        public static void WriteArray(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] != 0)
+                    {
+                        Console.Write($"{array[i, j]} ");
+                    }
+                    else Console.Write("  ");
+                }
+                System.Console.WriteLine(); ;
+            }
+        }
     }
-    System.Console.WriteLine();;
-  }
+
+    class PascalTriangle
+    {
+        public static void TransformationPascalTriangle(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int count = 0;
+                for (int j = array.GetLength(1) - 1; j >= 0; j--)
+                {
+                    if (array[i, j] != 0)
+                    {
+                        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
+                        array[i, j] = 0;
+                        count++;
+                    }
+                }
+            }
+            array[array.GetLength(0) - 1, 0] = 1;
+        }
+
+        public static void FillPascalTriangle(double[,] pascalTriangle)
+        {
+            for (int k = 0; k < pascalTriangle.GetLength(0); k++)
+            {
+                pascalTriangle[k, 0] = 1;
+            }
+            for (int i = 1; i < pascalTriangle.GetLength(0); i++)
+            {
+                for (int j = 1; j < i + 1; j++)
+                {
+                    pascalTriangle[i, j] = pascalTriangle[i - 1, j] + pascalTriangle[i - 1, j - 1];
+                }
+            }
+        }
+
+    }
 }
